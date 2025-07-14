ba488f1932699983c48488ea93279a7c566522bb
ilhamhafizha <muhammadilhamhafizha@gmail.com>
2025-07-14 10:49:37 +0700

    fix: vite and tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <h1 className="text-red-500"> Hai Tailwind</h1>
-      </div>
+      <h1 class="text-red-500">Hello world!</h1>
     </>
   );
 }
